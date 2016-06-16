410527ab5c2518d20f1e9e0dca21fd9beaaab8a3
gaofeng21cn <gaofeng21cn@hotmail.com>
2016-06-16 18:29:13 +0800

    add getFunctionalSpectrum

--- R/FunctionalSpectra.R
+++ R/FunctionalSpectra.R
@@ -72,4 +72,35 @@ getFunctionalSpectra <- function(eps, geneSets = MSigDB, cores = parallel::detec
   res
 }
 
+#' Generate Functional Spectrum
+#'
+#' This function generates functional spectrum for a single given gene expression profile.
+#'
+#' @param expressionProfile a named numeric vector containing gene expression profile
+#' @param geneSets a List containg gene sets (defaut = MSigDB v5.0)
+#' @param refExp a character indicating cancer typer according to TCGA's indentifier, or a named vector containing reference expression
+#' @param cores a integer indicating cpu cores used in parallel computing (defaut = all cores - 1)
+#' @return a numeric vector containing functional spectrum
+#' @export
+#' @examples
+#' getFunctionalSpectrum(ep)
+getFunctionalSpectrum <- function(expressionProfile, geneSets = MSigDB, refExp, logChange = F) {
+  expressionProfile <- unlist(expressionProfile)
+  if(!logChange) {
+    if(is.null(refExp)) stop("Must have a reference expression profile!")
+    if(is.character(refExp)) {
+      if(!(refExp %in% rownames(ref_eps))) stop(paste(refExp, "is not a support identifier of cancer types!\n  Please use one in :", paste(row.names(ref_eps), collapse = " ")))
+      refExp <- ref_eps[refExp, ]
+    }
+
+    common <- intersect(names(expressionProfile), names(refExp))
+    lm_res <- coef(lm(expressionProfile[common] ~ refExp[common]))
+    expressionProfile <- expressionProfile[common] - (refExp[common] * lm_res[2] + lm_res[1])
+  }
+  geneList <- preprocessGeneList(expressionProfile)
+
+  res <- sapply(1:length(geneSets), function(idx) calcEnrichmentScore(geneList, geneSets[[idx]]))
+  names(res) <- names(geneSets)
+  res
+}
 
