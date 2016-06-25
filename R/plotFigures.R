a36a929c5b0dfdc24f6ea58826ac7c2b1c78b8b0
gaofeng21cn <gaofeng21cn@hotmail.com>
2016-06-25 15:30:59 +0800

    update plot style

--- R/plotFigures.R
+++ R/plotFigures.R
@@ -6,17 +6,17 @@
 #' @param labels a vector containing subtyping labels of patients
 #' @param annot a character indicating the annotation showed up in the plot
 #' @param color a vector containing colors used for different subtypes
-#' @param font a numeric indicating the font size used in the plot
+#' @param font a character indicating the font used in the plot (default: "Arial")
 #' @param xlab a character indicating the label of x-axis (default: "Follow up (weeks)")
 #' @param ylab a character indicating the label of y-axis (default: "DFS (prob.)")
 #' @return a ggplot2 object of the plot
-#' @import ggplot2
+#' @import ggplot2 cowplot
 #' @export
 #' @examples
 #' clinical <- survival::Surv(t.rfs, e.rfs)
 #' color.cms <- c("#E69E00","#0070B0","#CA78A6", "#009C73")
 #' plotKMCurve(clinical, labels, "GSE39582", color.cms)
-plotKMCurve <- function(clinical, labels, annot = NULL, color = NULL, font = 10, xlab = "Follow up (weeks)", ylab = "DFS (prob.)"){
+plotKMCurve <- function(clinical, labels, annot = NULL, color = NULL, font = "Arial", xlab = "Follow up (weeks)", ylab = "DFS (prob.)"){
   obj <-  clinical ~ labels
   surv <- survival::survfit(obj)
   survstats <- survival::survdiff(obj)
@@ -32,15 +32,13 @@ plotKMCurve <- function(clinical, labels, annot = NULL, color = NULL, font = 10,
   }
 
   p <- GGally::ggsurv(surv, surv.col = color, cens.col = color, xlab = xlab, ylab = ylab) +
-    annotate("text", x = Inf, y = Inf, label = paste0("P==", fancy_scientific(survstats$p.value, 3)), hjust = 1.2, vjust = 2, size = font/2.5, parse = TRUE) +
-    theme_bw() +
-    theme(text = element_text(size=font),
-          legend.title=element_blank(),
-          axis.text=element_text(size=font),
-          axis.title=element_text(size=font))
+    annotate("text", family=font, x = Inf, y = Inf, label =
+               paste0("italic(P)==", fancy_scientific(survstats$p.value,3)), hjust = 1.2, vjust = 2,parse = TRUE) +
+    theme(text = element_text(family=font),
+          legend.title = element_blank())
 
   # annotate data set name
-  if(!is.null(annot)) p <- p + annotate("text", x = 0, y = 0, label = annot, hjust = 0, size = font/2.5, vjust = 0)
+  if(!is.null(annot)) p <- p + annotate("text", x = 0, y = 0, label = annot, hjust = 0, vjust = 0)
 
   p
 }
