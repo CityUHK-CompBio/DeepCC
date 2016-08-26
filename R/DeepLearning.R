696986338604427287f61eb701b2df8ffc404f73
GAO Feng <gaofeng21cn@hotmail.com>
2016-08-26 20:52:54 +0800

    add gpu version training

--- R/DeepLearning.R
+++ R/DeepLearning.R
@@ -5,22 +5,44 @@
 #' @param trainData a data.frame containing functional spectra of training data (each row presents one sample)
 #' @param trainLabels a character vector containing lables of training data
 #' @param hiddenLayers a numeric vector containing hidden layers architecture
-#' @param cores a integer indicating cpu cores used in parallel computing (defaut = all cores - 1)
-#' @return a trained h2o deep learning model
+#' @param cores a integer indicating cpu cores used in parallel computing or GPUs (defaut = all cores - 1)
+#' @param device a string indicating using CPU or GPU (defaut = "cpu")
+#' @return a trained h2o/mxnet deep learning model
 #' @export
 #' @examples
 #' trainDeepCCModel(tcga.fs, tcga.labels)
-trainDeepCCModel <- function(trainData, trainLabels, hiddenLayers=c(2000,500,120,30,10), cores = parallel::detectCores() - 1) {
+trainDeepCCModel <- function(trainData, trainLabels, hiddenLayers=c(2000,500,120,30,10), cores = NULL, device = "cpu") {
+  if(device == "gpu") {
+    device.gpu <- lapply(0:(cores-1), function(i) {
+      mx.gpu(i)
+    })
+
+    fs <- data.matrix(trainData[!is.na(labels), ])
+    labels <- as.factor(na.omit(trainLabels))
+
+    classifier <- mxnet::mx.mlp(fs, as.numeric(labels)-1, array.layout="rowmajor",
+                    hidden_node=hiddenLayers, out_node=length(levels(labels)),
+                    out_activation="softmax", num.round=100, eval.metric=mx.metric.accuracy,
+                    learning.rate=0.1,
+                    momentum=0.9,
+                    initializer=mx.init.uniform(0.07),
+                    device = device.gpu)
+  }
+    else {
+    if(is.null(cores)) cores <- parallel::detectCores() - 1
     h2o::h2o.init(nthreads = cores)
     train_sv <- h2o::as.h2o(data.frame(trainData, class=trainLabels)[!is.na(trainLabels), ])
-    classifier <- h2o::h2o.deeplearning(x = 1:length(MSigDB), y=length(MSigDB)+1,
-                                   training_frame = train_sv,
-                                   activation="Tanh",
-                                   epochs = 100,
-                                   hidden = hiddenLayers,
-                                   ignore_const_cols=F
+
+    classifier <- h2o::h2o.deeplearning(x = 1:ncol(trainData), y=ncol(trainData)+1,
+                                        training_frame = train_sv,
+                                        activation="Tanh",
+                                        epochs = 100,
+                                        hidden = hiddenLayers,
+                                        ignore_const_cols=F
     )
-    classifier
+  }
+
+  classifier
 }
 
 #' Get DeepCC Labels
