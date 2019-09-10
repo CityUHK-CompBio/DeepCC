3f841714e341d3b199f112187301da4a98a4d925
Feng GAO <gaofeng21cn@gmail.com>
2019-09-10 22:11:34 +0800

    keras

--- R/DeepLearning.R
+++ R/DeepLearning.R
@@ -4,47 +4,73 @@
 #'
 #' @param trainData a data.frame containing functional spectra of training data (each row presents one sample)
 #' @param trainLabels a character vector containing lables of training data
-#' @param hiddenLayers a numeric vector containing hidden layers architecture
-#' @param gpus a integer indicating how many GPUs used in parallel computing using GPUs (defaut = 0)
+#' @param epochs the number of epochs
+#' @param dropout dropout rate
+#' @param activation_func activation function
 #' @return a trained DeepCC model
 #' @export
 #' @examples
-#' trainDeepCCModel(tcga.fs, tcga.labels)
-trainDeepCCModel <- function(trainData, trainLabels, hiddenLayers=c(2000,500,120,30,10), gpus = 0, round = 30, optimizer = "sgd", seed=1) {
-  fs <- data.matrix(trainData[!is.na(trainLabels), ])
-  labels <- as.factor(na.omit(trainLabels))
-  levels <- levels(labels)
+#' train_DeepCC_model(tcga.fs, tcga.labels)
+train_DeepCC_model <- function(trainData, trainLabels, epochs = 100, dropout = 0.5, activation_func = "selu") {
 
-  if(gpus > 0) {
-    device <- lapply(0:(gpus-1), mxnet::mx.gpu)
-  } else {
-    device  <- mxnet::mx.cpu()
-  }
+  ind <- !is.na(y_train)
+  x_train <- trainData[ind, ]
+  y_train <- factor(trainLabels)
+  levels <- levels(y_train)
+  y_train <- to_categorical(as.numeric(y_train[ind]) - 1, 4)
 
-  mxnet::mx.set.seed(seed)
+  k_clear_session()
 
-  if(optimizer == "adadelta") {
-    classifier <- mxnet::mx.mlp(fs, as.numeric(labels)-1, array.layout="rowmajor",
-                                hidden_node=hiddenLayers, out_node=length(levels),
-                                out_activation="softmax", num.round=round, eval.metric=mx.metric.accuracy,
-                                optimizer = "adadelta", # adadelta is also good, w/o pre-setting learning rate and momentum
-                                activation = "tanh",
-                                initializer = mxnet::mx.init.Xavier(),
-                                device = device)
-  } else {
-    classifier <- mxnet::mx.mlp(fs, as.numeric(labels)-1, array.layout="rowmajor",
-                                hidden_node=hiddenLayers, out_node=length(levels),
-                                out_activation="softmax", num.round=round, eval.metric=mx.metric.accuracy,
-                                learning.rate=0.01,
-                                momentum=0.9,
-                                activation = "tanh",
-                                optimizer = "sgd",
-                                initializer = mxnet::mx.init.Xavier(),
-                                device = device)
-  }
+  init_methods <- "glorot_uniform"
+  model <- keras_model_sequential()
+  model %>%
+    layer_dense(units = 1024, activation = activation_func, input_shape = ncol(x_train), kernel_initializer = init_methods) %>%
+    layer_batch_normalization() %>%
+    layer_dropout(rate = dropout) %>%
+    layer_dense(units = 1024, activation = activation_func, kernel_initializer = init_methods) %>%
+    layer_batch_normalization() %>%
+    layer_dropout(rate = dropout) %>%
+    layer_dense(units = 256, activation = activation_func, kernel_initializer = init_methods) %>%
+    layer_batch_normalization() %>%
+    layer_dropout(rate = dropout) %>%
+    layer_dense(units = 64, activation = activation_func, kernel_initializer = init_methods) %>%
+    layer_batch_normalization() %>%
+    layer_dropout(rate = dropout) %>%
+    layer_dense(units = 10, activation = activation_func, kernel_initializer = init_methods) %>%
+    layer_dense(units = 4, activation = 'softmax')
 
-  list(classifier=mxnet::mx.serialize(classifier), levels=levels)
+
+  model %>% compile(
+    loss = 'categorical_crossentropy',
+    optimizer = optimizer_adam(),
+    metrics = c('accuracy')
+  )
+
+  history <- model %>% fit(
+    x_train, y_train,
+    epochs = epochs, batch_size = 1024,
+    #callbacks = callback_tensorboard("../logs/run_deepcc"),
+    view_metrics = F,
+    validation_split = 0
+  )
+
+  list(classifier=model, levels=levels)
 }
+
+#' @export
+save_DeepCC_model <- function(deepcc_model, prefix) {
+  deepcc_model$classifier %>% save_model_hdf5(filepath = paste0(prefix, ".hdf5"))
+  levels <- deepcc_model$levels
+  save(levels, file = paste0(prefix, ".RData"))
+}
+
+#' @export
+load_DeepCC_model <- function(prefix) {
+  load(file = paste0(prefix, ".RData"))
+  classifer <- keras::load_model_hdf5(filepath = paste0(prefix, ".hdf5"))
+  list(classifier = classifer, levels = levels)
+}
+
 
 #' Get DeepCC Labels
 #'
@@ -56,22 +82,30 @@ trainDeepCCModel <- function(trainData, trainLabels, hiddenLayers=c(2000,500,120
 #' @return a character vector containing lables of training data
 #' @export
 #' @examples
-#' getDeepCCLabels(deepcc.model, newdata.fs)
-getDeepCCLabels <- function(DeepCCModel, newData, cutoff=0.5, prob.mode=F){
-
-  res <- predict(DeepCCModel$classifier, newData, array.layout="rowmajor")
-
-  predicted <- apply(res, 2, function(z) {
-    if(max(z) >= cutoff ) {
+#' get_DeepCC_label(deepcc.model, newdata.fs)
+get_DeepCC_label <- function (DeepCCModel, newData, cutoff = 0.5, prob.mode = F, prob.raw = F)
+{
+  res <- keras::predict_proba(DeepCCModel$classifier, newData)
+  predicted <- apply(res, 1, function(z) {
+    if (max(z) >= cutoff) {
       which.max(z)
-    } else { NA }
+    }
+    else {
+      NA
+    }
   })
-  pred <- factor(predicted, levels=seq(length(DeepCCModel$levels)), labels=DeepCCModel$levels)
+  pred <- factor(predicted, levels = seq(length(DeepCCModel$levels)),
+                 labels = DeepCCModel$levels)
+  if (prob.mode) {
+    pred <- data.frame(DeepCC = as.character(pred), Probability = round(apply(res,
+                                                                              1, max), digits = 3))
+  }
 
-  if(prob.mode) pred <- data.frame(DeepCC=as.character(pred), Probability= round(apply(res, 2, max), digits = 3))
+  if (prob.mode & prob.raw) {
+    pred <- res
+  }
 
   pred
-  #predicted <- factor(apply(predict(DeepCCModel$classifier, newData, array.layout="rowmajor"), 2, which.max), levels=seq(length(DeepCCModel$levels)), labels=DeepCCModel$levels)
 }
 
 #' Get DeepCC prob mat
@@ -79,15 +113,12 @@ getDeepCCLabels <- function(DeepCCModel, newData, cutoff=0.5, prob.mode=F){
 #' @export
 #' @examples
 #' getDeepCCLabels(deepcc.model, newdata.fs)
-getDeepCCProb <- function(DeepCCModel, newData){
+get_DeepCC_prob <- function(DeepCCModel, newData){
 
-  res <- t(predict(DeepCCModel$classifier, newData, array.layout="rowmajor"))
-  #res <- data.frame(t(res), check.names = F)
+  res <- keras::predict_proba(DeepCCModel$classifier, newData)
   colnames(res) <- DeepCCModel$levels
 
   res
-
-  #predicted <- factor(apply(predict(DeepCCModel$classifier, newData, array.layout="rowmajor"), 2, which.max), levels=seq(length(DeepCCModel$levels)), labels=DeepCCModel$levels)
 }
 
 #' Get DeepCC Features
@@ -100,66 +131,10 @@ getDeepCCProb <- function(DeepCCModel, newData){
 #' @export
 #' @examples
 #' getDeepCCFeatures(deepcc.model, fs)
-getDeepCCFeatures <- function(DeepCCModel, fs){
+get_DeepCC_features <- function(DeepCCModel, fs){
 
-  ## modified from fdavidcl on github
-  ## ref: https://github.com/dmlc/mxnet/issues/2785
-
-  predictInternal <- function(model, X, ctx=NULL, layer, array.batch.size=128, array.layout="auto") {
-    # initialization stuff I probably don't care about ---------------------------
-    if (is.null(ctx)) ctx <- mxnet:::mx.ctx.default()
-    if (is.array(X) || is.matrix(X)) {
-      if (array.layout == "auto") {
-        array.layout <- mxnet:::mx.model.select.layout.predict(X, model)
-      }
-      if (array.layout == "rowmajor") {
-        X <- t(X)
-      }
-    }
-    # end initialization ---------------------------------------------------------
-    # iterator creation ----------------------------------------------------------
-    ## X iterates through the batches of input data
-    X <- mxnet:::mx.model.init.iter(X, NULL, batch.size=array.batch.size, is.train=FALSE)
-    X$reset()
-    if (!X$iter.next()) stop("Cannot predict on empty iterator")
-    dlist = X$value()
-    # end iterator creation ------------------------------------------------------
-    # executor creation ----------------------------------------------------------
-    ## mx.simple.bind defined in https://github.com/dmlc/mxnet/blob/master/R-package/R/executor.R#L5
-    ## internally calls mx.symbol.bind, defined in https://github.com/dmlc/mxnet/blob/master/R-package/src/executor.cc#L191
-    ### see also: https://github.com/dmlc/mxnet/blob/e7514fe1b3265aaf15870b124bb6ed0edd82fa76/R-package/demo/basic_executor.R
-    # pexec <- mxnet:::mx.simple.bind(model$symbol, ctx=ctx, data=dim(dlist$data), grad.req="null")
-    internals <- model$symbol$get.internals()
-    pexec <- mxnet:::mx.simple.bind(internals[[4 * layer]], ctx=ctx, data=dim(dlist$data), grad.req="null")
-    # end executor creation ------------------------------------------------------
-    # set up arg arrays ----------------------------------------------------------
-    internalArgParams <- model$arg.params[1:(2 * layer)]
-    # print(names(internalArgParams))
-
-    mxnet:::mx.exec.update.arg.arrays(pexec, internalArgParams, match.name=T)
-    mxnet:::mx.exec.update.aux.arrays(pexec, model$aux.params, match.name=T)
-    # end set up arg arrays ------------------------------------------------------
-    # the rest is left untouched -------------------------------------------------
-    packer <- mxnet:::mx.nd.arraypacker()
-    X$reset()
-    while (X$iter.next()) {
-      dlist = X$value()
-      mxnet:::mx.exec.update.arg.arrays(pexec, list(data=dlist$data), match.name=T)
-      mxnet:::mx.exec.forward(pexec, is.train=FALSE)
-      out.pred <- mxnet:::mx.nd.copyto(pexec$ref.outputs[[1]], mxnet:::mx.cpu())
-      padded <- X$num.pad()
-      oshape <- dim(out.pred)
-      ndim <- length(oshape)
-      packer$push(mxnet:::mx.nd.slice(out.pred, 0, oshape[[ndim]] - padded))
-    }
-    X$reset()
-    df <- t(packer$get())
-    rownames(df) <- colnames(X)
-    return(df)
-  }
-
-  model <- mxnet::mx.unserialize(DeepCCModel$classifier)
-  layer <- length(model$symbol$get.internals()$arguments)/2 - 2
-
-  predictInternal(model, fs, layer = layer, array.layout = "rowmajor", array.batch.size = 1)
+  model <- DeepCCModel$classifier
+  intermediate_layer_model <- keras::keras_model(model$input,
+                                          model$layers[[length(model$layers)-1]]$output)
+  predict(intermediate_layer_model, fs)
 }
